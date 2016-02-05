2efbe9d2ad677a76647726c2fd6b7a0b1c114329
Theo Martin <theo.martin22@icloud.com>
2016-02-05 23:55:09 +0100

    Modification catalogue

---
+++ js/fake_db_tempo.js
@@ -0,0 +1,106 @@
+var article01 = {
+	nom : "bouteille01",
+	description : "description01",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article02 = {
+	nom : "bouteille02",
+	description : "description02",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article03 = {
+	nom : "bouteille03",
+	description : "description03",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article04 = {
+	nom : "bouteille04",
+	description : "description04",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article05 = {
+	nom : "bouteille05",
+	description : "description05",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article06 = {
+	nom : "bouteille06",
+	description : "description06",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article07 = {
+	nom : "bouteille07",
+	description : "description07",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article08 = {
+	nom : "bouteille08",
+	description : "description08",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article09 = {
+	nom : "bouteille09",
+	description : "description09",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article10 = {
+	nom : "bouteille10",
+	description : "description10",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article11 = {
+	nom : "bouteille11",
+	description : "description11",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article12 = {
+	nom : "bouteille12",
+	description : "description12",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article13 = {
+	nom : "bouteille13",
+	description : "description13",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article14 = {
+	nom : "bouteille14",
+	description : "description14",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var article15 = {
+	nom : "bouteille15",
+	description : "description15",
+	photo : "http://placehold.it/150x150",
+	prix : 15.99,
+}
+
+var listeArticle = [article01, article02, article03, article04, article05, article06, article07, article08, article09, article10, article11, article12, article13, article14, article15];
