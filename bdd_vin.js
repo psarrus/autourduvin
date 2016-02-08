fbddd6e395d467bf6e86ccf35aa2891f001c740a
klementik damien <klementikdamien@gmail.com>
2016-02-08 14:16:53 +0100

    intégration fiche base de donné

---
+++ bdd_vin.js
@@ -0,0 +1,182 @@
+fiche1 ={
+  titre : "Noces d'argent",
+  millesime:"AOC CORBIÈRES Rouge Millésime 2010",
+  sol:"Argilo-calcaire",
+  cepages : "65 % Syrah – 35 % Mourvèdre",
+  degre:"14%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle.Longue macération.",
+  degustation : "Couleur chaude et profonde. Le nez est d’une remarquable complexité, avec un mélange de notes de truffes, de résine, d’épices variées : poivre, cannelle... associées à des senteurs de garrigue chaude. Très belle construction en bouche, avec beaucoup d’ampleur, du gras et une très belle construction tannique. Très longue et flatteuse persistance aromatique.",
+  potentieldegarde:"12 à 15 ans.",
+  temperatureduservice: "17° à 18°C.",
+  accordsculinaires:"Viandes grillées ou rôties. Gibier.Fromages doux.",
+}
+fiche2 ={
+  titre : "La Liesse",
+  millesime:"AOC CORBIÈRES Rouge Millésime 2010",
+  sol:"Argilo-calcaire",
+  cepages : "65 % Syrah – 35 % Mourvèdre",
+  degre:"14%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle.Longue macération.",
+  degustation : "Couleur chaude et profonde. Le nez est d’une remarquable complexité, avec un mélange de notes de truffes, de résine, d’épices variées : poivre, cannelle... associées à des senteurs de garrigue chaude. Très belle construction en bouche, avec beaucoup d’ampleur, du gras et une très belle construction tannique. Très longue et flatteuse persistance aromatique.",
+  potentieldegarde:"12 à 15 ans.",
+  temperatureduservice: "17° à 18°C.",
+  accordsculinaires:"Viandes grillées ou rôties. Gibier.Fromages doux.",
+}
+fiche3 ={
+  titre : "L'euphorie",
+  millesime:"AOC CORBIÈRES Rouge Millésime 2010",
+  sol:"Argilo-calcaire",
+  cepages : "65 % Syrah – 35 % Mourvèdre",
+  degre:"14%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle.Longue macération.",
+  degustation : "Couleur chaude et profonde. Le nez est d’une remarquable complexité, avec un mélange de notes de truffes, de résine, d’épices variées : poivre, cannelle... associées à des senteurs de garrigue chaude. Très belle construction en bouche, avec beaucoup d’ampleur, du gras et une très belle construction tannique. Très longue et flatteuse persistance aromatique.",
+  potentieldegarde:"12 à 15 ans.",
+  temperatureduservice: "17° à 18°C.",
+  accordsculinaires:"Viandes grillées ou rôties. Gibier.Fromages doux.",
+}
+fiche4 ={
+  titre : "Le Gargamelle",
+  millesime:"AOC CORBIÈRES Rouge Millésime 2010",
+  sol:"Argilo-calcaire",
+  cepages : "65 % Syrah – 35 % Mourvèdre",
+  degre:"14%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle.Longue macération.",
+  degustation : "Couleur chaude et profonde. Le nez est d’une remarquable complexité, avec un mélange de notes de truffes, de résine, d’épices variées : poivre, cannelle... associées à des senteurs de garrigue chaude. Très belle construction en bouche, avec beaucoup d’ampleur, du gras et une très belle construction tannique. Très longue et flatteuse persistance aromatique.",
+  potentieldegarde:"12 à 15 ans.",
+  temperatureduservice: "17° à 18°C.",
+  accordsculinaires:"Viandes grillées ou rôties. Gibier.Fromages doux.",
+}
+fiche5 ={
+  titre : "Nathalie & Marie",
+  millesime:"AOC CORBIÈRES Rouge Millésime 2010",
+  sol:"Argilo-calcaire",
+  cepages : "65 % Syrah – 35 % Mourvèdre",
+  degre:"14%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle.Longue macération.",
+  degustation : "Couleur chaude et profonde. Le nez est d’une remarquable complexité, avec un mélange de notes de truffes, de résine, d’épices variées : poivre, cannelle... associées à des senteurs de garrigue chaude. Très belle construction en bouche, avec beaucoup d’ampleur, du gras et une très belle construction tannique. Très longue et flatteuse persistance aromatique.",
+  potentieldegarde:"12 à 15 ans.",
+  temperatureduservice: "17° à 18°C.",
+  accordsculinaires:"Viandes grillées ou rôties. Gibier.Fromages doux.",
+}
+fiche6 ={
+  titre : "Thomas & Mathieu",
+  millesime:"AOC CORBIÈRES Rosé élevé en fût de chêne Millésime 2013"
+  sol:"Argilo-calcaire",
+  cepages : "45% Cinsault – 30% Syrah – 25% Grenache",
+  degre:"13%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle. Pressurage direct et thermorégulation.",
+  degustation : "olie couleur aux nuances de litchi.Nez assez original, bien parfumé avec des notes de framboise associées à une légère touche vanillée.Bouche structurée, pleine et ronde.",
+  potentieldegarde:"",
+  temperatureduservice: "8 à 10°C.",
+  accordsculinaires:"Vin rosé de repas. Grillades,poissons en sauce, viandes blanches.",
+}
+fiche7 ={
+  titre : "Pietrus",
+  millesime:"AOC CORBIÈRES Rosé élevé en fût de chêne Millésime 2013"
+  sol:"Argilo-calcaire",
+  cepages : "45% Cinsault – 30% Syrah – 25% Grenache",
+  degre:"13%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle. Pressurage direct et thermorégulation.",
+  degustation : "olie couleur aux nuances de litchi.Nez assez original, bien parfumé avec des notes de framboise associées à une légère touche vanillée.Bouche structurée, pleine et ronde.",
+  potentieldegarde:"",
+  temperatureduservice: "8 à 10°C.",
+  accordsculinaires:"Vin rosé de repas. Grillades,poissons en sauce, viandes blanches.",
+}
+fiche8 ={
+  titre : "Le Veyron",
+  millesime:"AOC CORBIÈRES Rosé élevé en fût de chêne Millésime 2013"
+  sol:"Argilo-calcaire",
+  cepages : "45% Cinsault – 30% Syrah – 25% Grenache",
+  degre:"13%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle. Pressurage direct et thermorégulation.",
+  degustation : "olie couleur aux nuances de litchi.Nez assez original, bien parfumé avec des notes de framboise associées à une légère touche vanillée.Bouche structurée, pleine et ronde.",
+  potentieldegarde:"",
+  temperatureduservice: "8 à 10°C.",
+  accordsculinaires:"Vin rosé de repas. Grillades,poissons en sauce, viandes blanches.",
+}
+fiche9 ={
+  titre : "Château du raisin sec",
+  millesime:"AOC CORBIÈRES Rosé élevé en fût de chêne Millésime 2013"
+  sol:"Argilo-calcaire",
+  cepages : "45% Cinsault – 30% Syrah – 25% Grenache",
+  degre:"13%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle. Pressurage direct et thermorégulation.",
+  degustation : "olie couleur aux nuances de litchi.Nez assez original, bien parfumé avec des notes de framboise associées à une légère touche vanillée.Bouche structurée, pleine et ronde.",
+  potentieldegarde:"",
+  temperatureduservice: "8 à 10°C.",
+  accordsculinaires:"Vin rosé de repas. Grillades,poissons en sauce, viandes blanches.",
+}
+fiche10={
+  titre : "Laetitia & Nadège",
+  millesime:"AOC CORBIÈRES Rosé élevé en fût de chêne Millésime 2013"
+  sol:"Argilo-calcaire",
+  cepages : "45% Cinsault – 30% Syrah – 25% Grenache",
+  degre:"13%vol.",
+  vignification : "Raisin issu d’une exploitation qualifiée au titre de l’agriculture raisonnée. Récolte manuelle. Pressurage direct et thermorégulation.",
+  degustation : "olie couleur aux nuances de litchi.Nez assez original, bien parfumé avec des notes de framboise associées à une légère touche vanillée.Bouche structurée, pleine et ronde.",
+  potentieldegarde:"",
+  temperatureduservice: "8 à 10°C.",
+  accordsculinaires:"Vin rosé de repas. Grillades,poissons en sauce, viandes blanches.",
+}
+fiche11 ={
+  titre : "Le demon de l'Evêque",
+  millésime : "AOC CORBIÈRES Blanc Millésime 2014",
+  sol:"Argilo-calcaire",
+  cepages : "70 % Bourboulenc - 30 % Grenache",
+  degre:"12%vol.",
+  vignification : "",
+  degustation : "Jolie couleur fraiche et de nuance or pâle. Bonne puissance aromatique avec des notes de fleurs blanches et de pêche. Bouche ronde et savoureuse avec une jolie persistance des arômes.",
+  potentieldegarde:"",
+  temperatureduservice: "6° - 8°C.",
+  accordsculinaires:"Apéritif, poissons grillés, fruits de mer.",
+}
+fiche12 ={
+  titre : "Lucie & Anne",
+  millésime : "AOC CORBIÈRES Blanc Millésime 2014",
+  sol:"Argilo-calcaire",
+  cepages : "70 % Bourboulenc - 30 % Grenache",
+  degre:"12%vol.",
+  vignification : "",
+  degustation : "Jolie couleur fraiche et de nuance or pâle. Bonne puissance aromatique avec des notes de fleurs blanches et de pêche. Bouche ronde et savoureuse avec une jolie persistance des arômes.",
+  potentieldegarde:"",
+  temperatureduservice: "6° - 8°C.",
+  accordsculinaires:"Apéritif, poissons grillés, fruits de mer.",
+}
+
+fiche13 ={
+  titre : "Le Jonis",
+  millésime : "AOC CORBIÈRES Blanc Millésime 2014",
+  sol:"Argilo-calcaire",
+  cepages : "70 % Bourboulenc - 30 % Grenache",
+  degre:"12%vol.",
+  vignification : "",
+  degustation : "Jolie couleur fraiche et de nuance or pâle. Bonne puissance aromatique avec des notes de fleurs blanches et de pêche. Bouche ronde et savoureuse avec une jolie persistance des arômes.",
+  potentieldegarde:"",
+  temperatureduservice: "6° - 8°C.",
+  accordsculinaires:"Apéritif, poissons grillés, fruits de mer.",
+}
+fiche14 ={
+  titre : "Le Soulier",
+  millésime : "AOC CORBIÈRES Blanc Millésime 2014",
+  sol:"Argilo-calcaire",
+  cepages : "70 % Bourboulenc - 30 % Grenache",
+  degre:"12%vol.",
+  vignification : "",
+  degustation : "Jolie couleur fraiche et de nuance or pâle. Bonne puissance aromatique avec des notes de fleurs blanches et de pêche. Bouche ronde et savoureuse avec une jolie persistance des arômes.",
+  potentieldegarde:"",
+  temperatureduservice: "6° - 8°C.",
+  accordsculinaires:"Apéritif, poissons grillés, fruits de mer.",
+}
+fiche15 ={
+  titre : "La Machine",
+  millésime : "AOC CORBIÈRES Blanc Millésime 2014",
+  sol:"Argilo-calcaire",
+  cepages : "70 % Bourboulenc - 30 % Grenache",
+  degre:"12%vol.",
+  vignification : "",
+  degustation : "Jolie couleur fraiche et de nuance or pâle. Bonne puissance aromatique avec des notes de fleurs blanches et de pêche. Bouche ronde et savoureuse avec une jolie persistance des arômes.",
+  potentieldegarde:"",
+  temperatureduservice: "6° - 8°C.",
+  accordsculinaires:"Apéritif, poissons grillés, fruits de mer.",
+}
+var liste = [fiche1,fiche2,fiche3,fiche4,fiche5,fiche6,fiche7,fiche8,fiche9,fiche10,fiche11,fiche12,fiche13,fiche14,fiche15,]
